e4cc8880f687e091630d5f71f5a8b45f7cef048f
Andrew Farmer <ahfarmer@gmail.com>
2017-07-27 21:51:33 -0700

    meow

--- packages/gatsby/src/redux/plugin-watcher.js
+++ packages/gatsby/src/redux/plugin-watcher.js
@@ -4,8 +4,21 @@ const { store, emitter } = require(`./index`)
 const apiRunnerNode = require(`../utils/api-runner-node`)
 const { graphql } = require(`graphql`)
 
+let dirty = false
+
 emitter.on(`CREATE_NODE`, action => {
   console.log(`plugin-watcher CREATE_NODE`, action.traceId)
+  dirty = true
+})
+
+emitter.on(`API_RUNNING_QUEUE_EMPTY`, action => {
+  console.log(`plugin-watcher API_RUNNING_QUEUE_EMPTY`)
+  if (!dirty) {
+    return
+  }
+  dirty = false
+
+  // TODO: compare pages before & after running create pages
 
   const graphqlRunner = (query, context = {}) => {
     const schema = store.getState().schema
@@ -14,7 +27,7 @@ emitter.on(`CREATE_NODE`, action => {
 
   apiRunnerNode(`createPages`, {
     graphql: graphqlRunner,
-    traceId: action.traceId,
+    traceId: `watch-createPages`,
     waitForCascadingActions: true,
   })
     .then((result) => {
